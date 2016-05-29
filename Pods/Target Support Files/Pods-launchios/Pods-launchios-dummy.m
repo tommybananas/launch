4ce37ab36f9a2f4b613806677cf5f75e6d31247a
Tom <tom@hardindd.com>
2016-05-29 14:13:24 -0500

    initial

---
+++ Pods/Target Support Files/Pods-launchios/Pods-launchios-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_launchios : NSObject
+@end
+@implementation PodsDummy_Pods_launchios
+@end
--- launchios/ViewController.swift
+++ launchios/ViewController.swift
@@ -7,12 +7,43 @@
 //
 
 import UIKit
+import FirebaseDatabase
+import MapKit
+
+var ref: FIRDatabaseReference!
+var messages: [FIRDataSnapshot]! = []
+private var _refHandle: FIRDatabaseHandle!
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        ref = FIRDatabase.database().reference()
+        // Listen for new messages in the Firebase database
+        _refHandle = ref.child("path").observeEventType(.ChildAdded, withBlock: { (snapshot) -> Void in
+            messages.append(snapshot)
+            let data = snapshot.value as! NSDictionary
+            let lat = data["lat"] as! NSString
+            let long = data["long"] as! NSString
+            let speed = data["speed"] as! NSString
+            let traveled = data["traveled"] as! NSString
+            let alt = data["alt"] as! NSString
+            let date = data["date"] as! NSString
+            
+            
+            var ann = MKPointAnnotation()
+            ann.coordinate = CLLocationCoordinate2DMake(lat.doubleValue, long.doubleValue)
+            ann.title = "\(alt) ft, \(speed) mph, \(traveled) miles \(date.componentsSeparatedByString(" ")[0])"
+            self.mapView.addAnnotation(ann)
+            
+            self.textView.text = "Alt: \(alt) ft, Speed: \(speed) mph, tot: \(traveled) miles   \(date.componentsSeparatedByString(" ")[0])\n\(self.textView.text)"
+            
+            print(messages)
+            print("\(lat), \(long)")
+        })
     }
 
     override func didReceiveMemoryWarning() {
